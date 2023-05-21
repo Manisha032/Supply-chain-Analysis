e592797393f3289daa1fa58c68e7d87d14d38dbf
manisha033 <manisha.singh213479@gmail.com>
2023-05-21 15:11:00 +0530

    Initial commit

---
+++ UI/dashboard.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import streamlit as st
+
+
+
+def load_supply_chain_data():
+    
+
+    return pd.read_csv('supply_chain_data.csv')
+
+st.subheader("It's a data analysis mini project")
+
+st.title("Supply Chain Analysis") 
